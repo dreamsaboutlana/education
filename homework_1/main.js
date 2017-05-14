291c94c514a3a698a656935532861b815659a8a1
dreamsaboutlana <dreamsaboutlana@gmail.com>
2017-05-14 13:18:44 +0300

    add object

---
+++ homework_1/main.js
@@ -0,0 +1,28 @@
+var phoneUser = {
+	id: 1,
+	firstName: 'Svitlana',
+	surname: 'Golovko',
+	patronymic: 'Anatolievna',
+	nickName: 'Lana',
+	age: 27,
+	birthDate:{
+		day: 31,
+		month: 'January',
+		year: 1990
+	},
+	phone: {
+		mobilePhone: 0951234567,
+		homePhone: 0951234567,
+		emergencyPhone: 0951234567
+	},
+	email: 'cool_user@gmail.com',
+	skype: 'say_when_why',
+	group: {
+		family: true,
+		work: false,
+		friends: true
+	},
+	webSite: undefined,
+	skill: ['HTML5', 'CSS3', 'Less', 'Sass', 'Bootstrap', 'Gulp', 'JQuery'],
+	balckListStatus: false
+}
