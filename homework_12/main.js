45d7213778b6636eba103cb6870975783213772a
dreamsaboutlana <Sveta@worker.local>
2017-06-21 15:13:03 +0300

    Add new classes

--- homework_12/main.js
+++ homework_12/main.js
@@ -55,7 +55,7 @@ function next() {
 function Http() {
   this.createServer = function(fn) {
     this.fn = fn;
-    this.fn.apply(fn, [ctx,next]);
+    this.fn.apply(fn, [ctx, next]);
     return this;
 
   };
@@ -65,10 +65,24 @@ function Http() {
     return Http;
   };
 }
-const server = new Http().createServer(function(ctx, next) {
-  console.log(ctx);
-  console.log(next);
-}).listen(3000, 'localhost');
+class OtherHttp {
+
+  createServer(fn) {
+    this.fn = fn;
+    this.fn.apply(fn, [ctx, next]);
+    return this;
+  }
+  listen(PORT, host) {
+    console.log(`Server running on https://${host}:${PORT}`);
+    this.createServer(this.fn);
+    return Http;
+  }
+
+}
+// const server = new Http().createServer(function(ctx, next) {
+//   console.log(ctx);
+//   console.log(next);
+// }).listen(3000, 'localhost');
 
 
 // TASK 1
@@ -80,6 +94,73 @@ const server = new Http().createServer(function(ctx, next) {
 //
 // Создать несколько экземпляров классов Worker и Student, вывести их в консоль.
 // Убедиться что они имеют поля родительского класса Human
+class OtherHuman {
+  constructor(name, age, gender, height, weight) {
+    this._name = name;
+    this._age = age;
+    this._gender = gender;
+    this._height = height;
+    this._weight = weight;
+  }
+}
+
+class OtherWorker extends OtherHuman {
+  constructor(name, age, gender, height, weight, jobPlace, payment) {
+    super(name, age, gender, height, weight);
+    this._jobPlace = jobPlace;
+    this._payment = payment;
+  }
+  working() {
+    console.log('I should working very often');
+  }
+}
+class OtherStudent extends OtherHuman {
+  constructor(name, age, gender, height, weight, studyPlace, scholarship) {
+    super(name, age, gender, height, weight);
+    this._studyPlace = studyPlace;
+    this._scholarship = scholarship;
+  }
+  watchingSerials() {
+    console.log('I love watching Serials');
+  }
+}
+
+let a = new OtherWorker('Sveta', 25, 'female', 173, 60, 'Google Int.', 8000);
+let b = new OtherStudent('Sveta', 25, 'female', 173, 60, 'Google Int.', 8000);
+
+
+function Human(name, age, gender, height, weight) {
+  this.name = name;
+  this.age = age;
+  this.gender = gender;
+  this.height = height;
+  this.weight = weight;
+}
+
+Worker.prototype = Object.create(Human.prototype);
+
+function Worker(name, age, gender, height, weight, jobPlace, payment) {
+  Human.call(this, name, age, gender, height, weight);
+  this.jobPlace = jobPlace;
+  this.payment = payment;
+}
+Worker.prototype.working = function() {
+  console.log('I should working very often');
+};
+
+Student.prototype = Object.create(Human.prototype);
+
+function Student(name, age, gender, height, weight, studyPlace, scholarship) {
+  Human.call(this, name, age, gender, height, weight);
+  this.studyPlace = studyPlace;
+  this.scholarship = scholarship;
+}
+Student.prototype.watchingSerials = function() {
+  console.log('I love watching Serials');
+}
+
+var SvetaWorker = new Worker('Sveta', 25, 'female', 173, 60, 'Google Int.', 8000);
+var JoStudent = new Student('Jo', 19, 'male', 180, 90, 'KhPI', 150);
 
 
 // @SUPER
@@ -89,3 +170,9 @@ const server = new Http().createServer(function(ctx, next) {
 // Создайте функцию обертку над другой функцией
 //Каждый раз при вызове внутренней функции в консоле будут отображаться аргументы функции
 //которую мы обернули
+
+function wrap(...args) {
+  return (function inner() {
+    console.log(args);
+  })();
+}
