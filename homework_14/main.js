a467b0ee1302a448832c1b8772783683f332c58c
dreamsaboutlana <Sveta@worker.local>
2017-07-03 10:04:58 +0300

    Changing last task

--- homework_14/main.js
+++ homework_14/main.js
@@ -2,7 +2,7 @@
 
 //TASK 0. Найдите числа которые повторяются нечетное количество раз в массиве
 
-function uniqueElem(arr) {
+function solution(arr) {
 
   let result = {};
   let nArr = [];
@@ -25,11 +25,15 @@ function uniqueElem(arr) {
   return nArr;
 
 }
-// console.log(uniqueElem([12, 23, 34, 12, 12, 23, 12, 45])); //-> [34 45]
-// console.log(uniqueElem([4, 4, 100, 5000, 4, 4, 4, 4, 4, 100, 100, ])); // -> [4 100 5000]
-// console.log(uniqueElem([3, 3, 4, 6, 4, 5, 9, 9, 21, 9])); //-> [6 5 9 21]
-// console.log(uniqueElem([4, 8, 15, 16, 23, 42, 4, 15, 42, 42])); //-> [8 16 23 42]
-// console.log(uniqueElem([2, 2, 44, 44])); // => []
+const solution2 = arr => arr.filter(number => {
+  return arr.filter(elem => elem === number).length % 2;
+}).filter((elem, index, arr) => arr.indexOf(elem) === index);
+
+// console.log(solution([12, 23, 34, 12, 12, 23, 12, 45])); //-> [34 45]
+// console.log(solution([4, 4, 100, 5000, 4, 4, 4, 4, 4, 100, 100, ])); // -> [4 100 5000]
+// console.log(solution([3, 3, 4, 6, 4, 5, 9, 9, 21, 9])); //-> [6 5 9 21]
+// console.log(solution([4, 8, 15, 16, 23, 42, 4, 15, 42, 42])); //-> [8 16 23 42]
+// console.log(solution([2, 2, 44, 44])); // => []
 
 
 const someWebpackModule = `module.exports = {
@@ -92,70 +96,88 @@ TASK 3
 
 */
 
-// let app = {
-//   createTag(tag) {
+const app = {
+  appName: 'Тест по програмированию',
+  questions: [{
+    name: 'Что делает оператор ===?',
+    answers: ['Сравнивает по ссылке, а не по значению',
+      'Сравнивает без приведения типа',
+      'Нет такого оператора'
+    ],
+    rightAnswers: [1]
+  }, {
+    name: 'Что получится, если сложить true + false?',
+    answers: ['truefalse', '0', 1],
+    rightAnswers: [2]
+  }, {
+    name: 'Чему равно значение выражения 4 - "5" + 0xf - "1e1"?',
+    answers: ['Цифре', 'Строке', 'NaN'],
+    rightAnswers: [0]
+  }],
 
-//   }
-// };
+  createLi() {
+    return document.createElement('li');
+  },
 
-let create = tag => document.createElement(tag);;
+  createQuestions(question) {
 
-let h1 = create('h1');
-h1.textContent = 'Тест по програмированию';
-document.body.appendChild(h1);
+    const li = this.createLi();
+    const ul = this.new('ul');
+    const h3 = this.new('h3');
 
-let form = create('form');
+    h3.textContent = question.name;
 
-// let label = create('label');
-var labelInfo = ['Что делает оператор ===?',
-  'Что получится, если сложить true + false?',
-  'Чему равно значение выражения 4 - "5" + 0xf - "1e1"?'
-];
-var inputInfo = [
-  ['Сравнивает по ссылке, а не по значению',
-    'Сравнивает без приведения типа',
-    'Нет такого оператора'
-  ],
-  ['truefalse', '0', 1, 'NaN'],
-  ['Цифре', 'Строке', 'NaN']
-];
+    question.answers.forEach(answer => {
+      const htmlAnswer = this.createAnswer(answer);
+      ul.appendChild(htmlAnswer);
+    });
 
+    li.appendChild(h3);
+    li.appendChild(ul);
 
-labelInfo.forEach((elem, index) => {
+    return li;
+  },
+  createAnswer(answer) {
 
-  const div = create('div');
-  div.setAttribute("class", "form_block");
+    const li = this.createLi();
+    const label = this.new('label');
+    const input = this.new('input');
 
-  const legend = create('legend');
-  div.appendChild(legend);
-  legend.textContent = elem;
+    label.textContent = answer;
+    input.setAttribute('type', 'checkbox');
 
-  inputInfo[index].forEach(val => {
-    const label = create('label');
-    const span = create('span');
+    li.appendChild(input);
+    li.appendChild(label);
 
-    const input = create('input');
-    input.setAttribute("type", "checkbox");
+    return li;
+  },
+  render() {
 
-    label.appendChild(input);
-    label.appendChild(span);
+    const body = document.body;
+    const main = this.new('main');
+    const h1 = this.new('h1');
+    const ol = this.new('ol');
+    const button = this.new('button');
 
-    span.textContent = val;
-    div.appendChild(label);
-  });
-
-  form.appendChild(div);
-});
-const div = create('div');
-div.setAttribute("class", "form_block");
-let button = create('input');
-button.setAttribute("type", "submit");
-button.setAttribute("value", "Send");
-div.appendChild(button);
-form.appendChild(div);
-
-document.body.appendChild(form);
+    h1.textContent = this.appName;
+    button.textContent = 'Submit';
 
 
+    main.appendChild(h1);
+    main.appendChild(ol);
 
-// app.render();
+    this.questions.forEach(question => {
+      const htmlQuestion = this.createQuestions(question);
+      ol.appendChild(htmlQuestion);
+    });
+
+    main.appendChild(button);
+    body.appendChild(main);
+  },
+
+  new(tag) {
+    return document.createElement(tag);
+  }
+}
+
+app.render();
